fd62d16e6ebe7fcf84eadf0cc2bba476320bfbbf
sultana <sultanaisgneiss@gmail.com>
2016-07-07 21:56:48 +0000

    adding assignment 4 files

---
+++ assignment_4/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* Write "hello, world\n" to stdout. Return 0. */
+int main(void)
+{
+   printf("hello, world\n");
+   return 0;
+}
---
+++ assignment_4/loop.py
@@ -0,0 +1,3 @@
+#print 1-10 on separate lines
+for i in range(10):
+    print(i)
